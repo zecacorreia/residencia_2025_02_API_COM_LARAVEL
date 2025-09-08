c458894903f149b5133ff94e06d64636d8fb7cff
Luiz Siqueira <lsiqueira@sjcc.com.br>
2025-09-08 20:12:03 -0300

    crud agenda

--- src/app/Http/Controllers/AgendaController.php
+++ src/app/Http/Controllers/AgendaController.php
@@ -11,6 +11,70 @@ use App\Http\Requests\AgendaRequest;
 class AgendaController extends Controller
 {
 
+    public function index(){
+        $agenda = Agenda::all();   //select * from agendas
+        return response()->json($agenda, 200);
+    }
+
+    public function criar(Request $request){
+        $nome_recebido = $request->input('nome');
+        $telefone_recebido = $request->input('telefone');
+
+        $agenda = new Agenda();
+        $agenda->nome = $nome_recebido;
+        $agenda->telefone = $telefone_recebido;
+        $agenda->save();
+
+        return response()->json($agenda, 201);
+
+    }
+
+    //visualizar
+    public function visualizar($id){
+        $registro = Agenda::find($id);
+
+        if($registro){
+            return response()->json($registro, 200);
+        }
+
+        return response()->json(['erro' => 'Registro nao encontrado'], 404);
+
+    }
+
+
+
+    //atualizar
+    public function atualizar(Request $request, $id){
+        $nome_recebido = $request->input('nome');
+        $telefone_recebido = $request->input('telefone');
+
+        $agenda = Agenda::find($id);
+
+        if(!$agenda){
+            return response()->json(['erro' => 'Registro nao encontrado'], 404);
+        }
+
+        $agenda->nome = $nome_recebido;
+        $agenda->telefone = $telefone_recebido;
+        $agenda->save();
+
+        return response()->json($agenda, 200);
+
+    }
+
+    //deletar
+    public function deletar($id){
+        $registro = Agenda::find($id);
+
+        if(!$registro){
+            return response()->json(['erro' => 'Registro nao encontrado'], 404);
+        }
+
+        $registro->delete();
+
+        return response()->json(['msg' => 'Registro deletado com sucesso'], 200);
+    }
+
 
 
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Tokencontroller;
 
+use App\Http\Controllers\AgendaController;
+
+
+
+
+Route::get('/agenda', [AgendaController::class, 'index']);  //200
+Route::post('/agenda', [AgendaController::class, 'criar']);  //201
+Route::get('/agenda/{id}', [AgendaController::class, 'visualizar']); //200 ou 404
+Route::put('/agenda/{id}', [AgendaController::class, 'atualizar']); //200 ou 404
+Route::delete('/agenda/{id}', [AgendaController::class, 'deletar']); //200 ou 404
+
 
 
 
