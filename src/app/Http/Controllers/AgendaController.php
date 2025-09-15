930a6ebaba96efeca629577a8cb0633efeb25752
Luiz Siqueira <lsiqueira@sjcc.com.br>
2025-09-15 20:34:25 -0300

    revisao do crud

--- src/app/Http/Controllers/AgendaController.php
+++ src/app/Http/Controllers/AgendaController.php
@@ -11,14 +11,50 @@ use App\Http\Requests\AgendaRequest;
 class AgendaController extends Controller
 {
 
+    //listar
     public function index(){
-        $agenda = Agenda::all();   //select * from agendas
+        // $agenda = Agenda::all();   //select * from agendas.
+        $agenda = Agenda::limit(10)->get();   //select * from agendas
         return response()->json($agenda, 200);
     }
 
+    //criar
     public function criar(Request $request){
         $nome_recebido = $request->input('nome');
         $telefone_recebido = $request->input('telefone');
+
+
+        //INICIO DAS VALIDAÇÕES MANUAIS
+            //validar nome e obrigatorio
+            if(!$nome_recebido){
+                return response()->json(['erro' => 'Nome e obrigatorio'], 400);
+            }
+
+            //validar telefone e obrigatorio
+            if(!$telefone_recebido){
+                return response()->json(['erro' => 'Telefone e obrigatorio'], 400);
+            }
+
+            //validar telefone minimo 8 caracteres
+            if(strlen($telefone_recebido) < 8){
+                return response()->json(['erro' => 'Telefone invalido'], 400);
+            }
+
+
+            //validar nome unico
+            $valida_nome = Agenda::where('nome', $nome_recebido)->first();
+
+            if($valida_nome){
+                return response()->json(['erro' => 'Nome ja cadastrado'], 400);
+            }
+
+            //validar telefone unico
+            $valida_telefone = Agenda::where('telefone', $telefone_recebido)->first();
+            if($valida_telefone){
+                return response()->json(['erro' => 'Telefone ja cadastrado'], 400);
+            }
+        //FIM DAS VALIDAÇÕES MANUAIS
+
 
         $agenda = new Agenda();
         $agenda->nome = $nome_recebido;
@@ -42,7 +78,6 @@ class AgendaController extends Controller
     }
 
 
-
     //atualizar
     public function atualizar(Request $request, $id){
         $nome_recebido = $request->input('nome');
@@ -53,6 +88,46 @@ class AgendaController extends Controller
         if(!$agenda){
             return response()->json(['erro' => 'Registro nao encontrado'], 404);
         }
+
+
+
+        //INICIO DAS VALIDAÇÕES MANUAIS
+            //validar nome e obrigatorio
+            if(!$nome_recebido){
+                return response()->json(['erro' => 'Nome e obrigatorio'], 400);
+            }
+
+            //validar telefone e obrigatorio
+            if(!$telefone_recebido){
+                return response()->json(['erro' => 'Telefone e obrigatorio'], 400);
+            }
+
+            //validar telefone minimo 8 caracteres
+            if(strlen($telefone_recebido) < 8){
+                return response()->json(['erro' => 'Telefone invalido'], 400);
+            }
+
+
+            //validar nome unico
+            // $valida_nome = Agenda::where('nome', $nome_recebido)->first();
+
+            // if($valida_nome){
+            //     return response()->json(['erro' => 'Nome ja cadastrado'], 400);
+            // }
+
+            //validar telefone unico
+            // $valida_telefone = Agenda::where('telefone', $telefone_recebido)->first();
+            // if($valida_telefone){
+            //     return response()->json(['erro' => 'Telefone ja cadastrado'], 400);
+            // }
+        //FIM DAS VALIDAÇÕES MANUAIS
+
+
+
+
+
+
+
 
         $agenda->nome = $nome_recebido;
         $agenda->telefone = $telefone_recebido;
